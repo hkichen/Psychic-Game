df6835947fdc652833da73398c38f52825f53018
hkichen <hkichen@gmail.com>
2018-02-26 17:30:24 -0500

    need to write how to reset game when losing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,20 +4,29 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 var wins = 0;
 var losses = 0;
 var guesses = 10;
-    
+
+var computerChoice = letters[Math.floor(Math.random() * letters.length)];
+    console.log(computerChoice);
+
 document.onkeyup = function(event) {
     var userGuess = event.key;
         
-    //if user guesses same as computer, then computer chooses new letter
-    if (userGuess === computerChoice) {
-    var computerChoice = letters[Math.floor(Math.random() * letters.length)];
-    
-    console.log(computerChoice);
+    if (userGuess === computerChoice
+    ) {
+        wins = wins + 1
+    } else {
+        guesses = guesses - 1
+    }
+    if (guesses === 0
+    ) {
+        losses = losses + 1
+    }
             
-    if(userGuess === computerChoice){wins++;}else{guesses--;}
-    if(guesses === 0){losses++;}
-            
-    var html = "<p>You chose: " + userGuess + "</p>" + "<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>" + "<p>Guesses: " + guesses + "</p>"; document.querySelector("#game".innerHTML = html);
-        }
-};
-    
+    var html = "<p>" + "You chose: " + userGuess + "</p>" + "<p>" + "Wins: " + wins + "</p>" + "<p>" + "Losses: " + losses + "</p>" + "<p>" + "Guesses: " + guesses + "</p>"; document.querySelector("#game").innerHTML = html;
+}
+function clearFields() {
+    userGuess.value = "";
+    guesses = 10;
+    computerChoice = letters[Math.floor(Math.random() * letters.length)];
+}
+
